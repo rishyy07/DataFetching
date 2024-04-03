66d7f1626abd61b546f3cda0c5e1ad0d759622f8
Rishita Sharma <rishita4477@gmail.com>
2024-04-04 00:35:36 +0530

    files added

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [posts, setPosts] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      let response = await fetch("https://jsonplaceholder.typicode.com/posts")
+      let data = await response.json();
+      console.log(data);
+      setPosts(data);
+      //alternative
+      //fetch("https://jsonplaceholder.typicode.com/posts")
+      //.then((response)=>{
+      //  return response.json()
+      //}).then((data)=>{
+      //console.log(data);
+      //}).catch((err)=>{console.log(err)
+      //})
+    }
+    fetchData()
+  })
+
+
+  return (
+    <>
+      <h1> Fetch Data </h1>
+      <div className="container">
+        {posts && posts.map((post) => {
+          return (
+            <Onepost title={post.title} body={post.body} />
+          )
+        })}
+
+      </div>
+    </>
+  )
+}
+
+function Onepost(props) {
+  return (
+    <>
+      <li>
+
+      </li>
+    </>
+  )
+}
+
+export default App
